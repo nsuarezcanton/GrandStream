185b03f59f982e255a59cd0be7a1f18fee812cb9
Nico Suarez-Canton Trueba <nsuarez.canton@gmail.com>
2017-06-05 17:56:43 -0400

    Initial Commit

---
+++ GrandStream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrandStream
+//
+//  Created by Nicolas Suarez-Canton Trueba on 6/5/17.
+//  Copyright © 2017 Nicolas Suarez-Canton Trueba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
